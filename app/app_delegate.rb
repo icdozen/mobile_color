58e2318e71041f845a494d7b406c0ee36d606d13
icdozen <icdozen@gmail.com>
2016-04-03 14:53:31 -0400

    delegate UIwindow/UINavigationController

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,15 +1,28 @@
+# class AppDelegate
+#   def application(application, didFinishLaunchingWithOptions:launchOptions)
+#     rootViewController = UIViewController.alloc.init
+#     rootViewController.title = 'Colr'
+#     rootViewController.view.backgroundColor = UIColor.whiteColor
+
+#     navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+
+#     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+#     @window.rootViewController = navigationController
+#     @window.makeKeyAndVisible
+
+#     true
+#   end
+# end
+
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'Colr'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
-
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
-
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
-    @window.makeKeyAndVisible
 
+    @search_controller = SearchController.alloc.initWithNibName(nil, bundle:nil)
+    @navigation_controller = UINavigationController.alloc.initWithRootViewController(@search_controller)
+
+    @window.rootViewController = @navigation_controller
+    @window.makeKeyAndVisible
     true
   end
 end
